1a4b7fa0f9437455e8c1efd84dc6c8d4d645d875
David Smith <david.smith@pagely.com>
2024-10-25 11:29:08 -0500

    account:collabs:ls command

---
+++ src/Command/Accounts/ListCollabsCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Pagely\AtomicClient\Command\Accounts;
+
+use Pagely\AtomicClient\API\Accounts\AccountsClient;
+use Pagely\AtomicClient\API\AuthApi;
+use Pagely\AtomicClient\Command\Command;
+use Pagely\AtomicClient\Command\OauthCommandTrait;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ListCollabsCommand extends Command
+{
+    use OauthCommandTrait;
+
+    /**
+     * @var AccountsClient
+     */
+    protected $api;
+
+    public function __construct(AuthApi $authApi, AccountsClient $apps, $name = 'account:collabs:ls')
+    {
+        $this->authClient = $authApi;
+        $this->api = $apps;
+        parent::__construct($name);
+    }
+
+    public function configure()
+    {
+        parent::configure();
+        $this
+            ->setDescription('List collaborators on account')
+            ->addArgument('accountId', InputArgument::REQUIRED, 'Account ID')
+        ;
+        $this->addOauthOptions();
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $accountId = $input->getArgument('accountId');
+        $token = $this->token->token;
+
+        $r = $this->api->listAccountCollaborators($token, $accountId);
+        $output->writeln(json_encode(json_decode($r->getBody()->getContents()), JSON_PRETTY_PRINT));
+
+        return 0;
+    }
+}
